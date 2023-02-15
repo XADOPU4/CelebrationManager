27ff9e6da4e8aead55e1197b854dd7b1118fc14d
Daniil Xadopu4 <danya.khadorich@yandex.ru>
2023-02-15 15:04:13 +0300

    Add project template

---
+++ src/main/java/com/eventmanager/coreservicediploma/CoreServiceDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.eventmanager.coreservicediploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreServiceDiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreServiceDiplomaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventmanager/coreservicediploma/CoreServiceDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventmanager.coreservicediploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
